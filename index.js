90f890f20b399380f7a5eab13be0c87e686945ed
dev <dev@ntb-dev>
2018-03-02 19:57:13 -0300

    Aula 4 - Primeira autenticaçao - HTTP basic

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const passport = require('passport')
+const BasicStrategy = require('passport-http').BasicStrategy
 const app = express();
+
+/** PASSPORT BASIC **/
+passport.use(require('./src/auth/basic'))
 
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(passport.initialize())
+
+app.get('*', passport.authenticate('basic',{ session: false }))
 
 require('./src/index')(app)
 
