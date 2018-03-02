1feeb9da5b48ede3c9b985ef69afb615e946b046
dev <dev@ntb-dev>
2018-03-02 20:23:57 -0300

    Aula 5 - Iniciando autenticaçao local (aula passada - aula 4)$ npm install passport passport-http --save
    (aula 5)$ npm install passport passport-local express-session --save
    
    (aula 5)$ npm install mongoose pug --save

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 const passport = require('passport')
 const BasicStrategy = require('passport-http').BasicStrategy
 const app = express();
 
 /** PASSPORT BASIC **/
-passport.use(require('./src/auth/basic'))
+//passport.use(require('./src/auth/basic'))
+//app.get('*', passport.authenticate('basic',{ session: false }))
 
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(passport.initialize())
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname,'src/view'))
 
-app.get('*', passport.authenticate('basic',{ session: false }))
 
 require('./src/index')(app)
+
+mongoose.connect('mongodb://localhost:27017/auth')
 
 //listen(porta, host, callback)
 //quando não passa nada no host () ele entende como host local.
--- src/controller/main/main.js
+++ src/controller/main/main.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
-    return res.json({
-        msg: 'Hello from Express.js + Passport '
-    })
+    //return res.json({
+        //msg: 'Hello from Express.js + Passport '
+    //})
+    return res.render('main/index')
 }
