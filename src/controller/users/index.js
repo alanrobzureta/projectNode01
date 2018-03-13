f50689a0ce5594d57ac054a7a3c993e7a58a03d9
dev <dev@ntb-dev>
2018-03-12 21:26:24 -0300

    Finalizando a criação de um novo usuario (erro ao cadastrar)

--- src/controller/users/index.js
+++ src/controller/users/index.js
@@ -1,13 +1,7 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-<<<<<<< HEAD
-router.post('/', require('./create'))
-router.get('/new', require('./new'))
+router.post('/', require('./create'));
+router.get('/new', require('./new'));
 
-=======
-
-router.get('/new', require('./new'))
->>>>>>> 090ee181262a2203c56cb6f511622487b55582c8
-
-module.exports = router
+module.exports = router;
